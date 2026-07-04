bbefd41d34c7cb846979c21afbc9a7adbb2c891c
crannnyr <grandswisssm@gmail.com>
2026-07-04 11:33:31 +0100

    Update StaffListItem.tsx

--- src/pages/dashboard/StoreSettings/Staff/StaffListItem.tsx
+++ src/pages/dashboard/StoreSettings/Staff/StaffListItem.tsx
@@ -137,7 +137,7 @@ export default function StaffListItem({ staff, isMutating, onRemove, onResend, o
                   onClick={() => onTogglePermission(staff.id, perm.key, !checked)}
                   className={`relative w-11 h-6 rounded-full transition-colors flex-shrink-0 ${checked ? 'bg-orange-500' : 'bg-gray-200'}`}
                 >
-                  <span className={`absolute top-0.5 w-5 h-5 bg-white rounded-full shadow transition-transform ${checked ? 'translate-x-5' : 'translate-x-0.5'}`} />
+                  <span className={`absolute left-0.5 top-0.5 w-5 h-5 bg-white rounded-full shadow transition-transform ${checked ? 'translate-x-5' : 'translate-x-0'}`} />
                 </button>
               </div>
             );
