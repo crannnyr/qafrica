43e63988bed3ca380b32e3e45140ee22c4e23b7e
crannnyr <grandswisssm@gmail.com>
2026-09-25 23:10:25 +0100

    add Import Admin v2 pricing shipping route

--- src/App.tsx
+++ src/App.tsx
@@ -56,6 +56,7 @@ const ImportAdminV2Dashboard = lazy(() => import('@/pages/import-admin-v2/Manage
 const ImportAdminV2Products = lazy(() => import('@/pages/import-admin-v2/ManagementProducts'));
 const ImportAdminV2Orders = lazy(() => import('@/pages/import-admin-v2/ManagementOrders'));
 const ImportAdminV2Settings = lazy(() => import('@/pages/import-admin-v2/ManagementSettings'));
+const ImportAdminV2PricingShipping = lazy(() => import('@/pages/import-admin-v2/ManagementPricingShipping'));
 const ImportAdminV2Categories = lazy(() => import('@/pages/import-admin-v2/ManagementCategories'));
 const ImportAdminV2Expenses = lazy(() => import('@/pages/import-admin-v2/ManagementExpenses'));
 const ImportAdminV2ProductAdd = lazy(() => import('@/pages/import-admin-v2/ManagementProductAdd'));
@@ -372,6 +373,7 @@ function App() {
             <Route path="orders" element={<ImportAdminV2Orders />} />
             <Route path="categories" element={<ImportAdminV2Categories />} />
             <Route path="settings" element={<ImportAdminV2Settings />} />
+            <Route path="pricing-shipping" element={<ImportAdminV2PricingShipping />} />
             <Route path="expenses" element={<ImportAdminV2Expenses />} />
             <Route path="products/add" element={<ImportAdminV2ProductAdd />} />
             <Route path="products/edit/:id" element={<ImportAdminV2ProductAdd />} />
