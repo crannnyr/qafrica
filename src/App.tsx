dd5f772fd8cff2333a60cc91e7985261d77646a4
crannnyr <grandswisssm@gmail.com>
2026-09-26 05:35:15 +0100

    feat: add email templates route to import admin v2

--- src/App.tsx
+++ src/App.tsx
@@ -66,6 +66,7 @@ const ImportAdminV2CustomOrders = lazy(() => import('@/pages/import-admin-v2/Imp
 const ImportAdminV2Refunds = lazy(() => import('@/pages/import-admin-v2/ImportAdminV2Refunds'));
 const ImportAdminV2PaymentRecovery = lazy(() => import('@/pages/import-admin-v2/ImportAdminV2PaymentRecovery'));
 const ImportAdminV2PaymentTransactions = lazy(() => import('@/pages/import-admin-v2/ImportAdminV2PaymentTransactions'));
+const ImportAdminV2EmailTemplates = lazy(() => import('@/pages/import-admin-v2/ImportAdminV2EmailTemplates'));
 const ImportAdminV2ProductAdd = lazy(() => import('@/pages/import-admin-v2/ManagementProductAdd'));
 const ImporterDashboardPage = lazy(() => import('@/pages/recommendations/ImporterDashboardPage'));
 const RecommendationsPage = lazy(() => import('@/pages/recommendations/RecommendationsPage'));
@@ -389,6 +390,7 @@ function App() {
             <Route path="refunds" element={<ImportAdminV2Refunds />} />
             <Route path="payment-recovery" element={<ImportAdminV2PaymentRecovery />} />
             <Route path="payment-transactions" element={<ImportAdminV2PaymentTransactions />} />
+            <Route path="email-templates" element={<ImportAdminV2EmailTemplates />} />
             <Route path="products/add" element={<ImportAdminV2ProductAdd />} />
             <Route path="products/edit/:id" element={<ImportAdminV2ProductAdd />} />
           </Route>
