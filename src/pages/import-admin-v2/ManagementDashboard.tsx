1edb464d0b403ae1904fbc7550c2befba53fc79f
crannnyr <grandswisssm@gmail.com>
2026-09-26 07:44:35 +0100

    Fix duplicate ManagementAnalytics import

--- src/pages/import-admin-v2/ManagementDashboard.tsx
+++ src/pages/import-admin-v2/ManagementDashboard.tsx
@@ -1,8 +1,7 @@
 import { User, ShieldCheck } from 'lucide-react';
 import TrendingManager from '@/pages/import-admin/TrendingManager';
 import { getManagementToken } from './ManagementAuth';
-import ManagementAnalytics from './ManagementAnalytics';
-import { useImportAdminPermissions } from '@/hooks/useImportAdminPermissions';
+import { useImportAdminPermissions from '@/hooks/useImportAdminPermissions';
 import { getManagementManager } from './ManagementAuth';
 import ManagementAnalytics from './ManagementAnalytics';
 
@@ -26,7 +25,6 @@ export default function ManagementDashboard() {
         <div>
           <TrendingManager token={token} />
 
-      <ManagementAnalytics />
         </div>
       )}
 
